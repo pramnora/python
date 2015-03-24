52c6b36c01b75aa95e67f47af1921e7e1da0c1ed
Paul Ramnora <paul@ramnora.com>
2015-03-24 16:50:55 +0000

    Update array01.py
    Python array used to store numeric literals, only

--- array01.py
+++ array01.py
@@ -1,23 +1,17 @@
+# NOTE: In Python you don't need to declare the data type, first; instead, data type is automatically inferred.
+
+# python array data is enclosed inside of a pair of square brackets: [];
+# and, each new data item is seperated from the next data item by the use of a comma: [,,,]
+                             
 num = [1,10,100,1000,10000]
+
+# a for loop can be used to traverse through the entire array;
+# the print out of each data item is achieved by using the for loop variable name
+
 for eachArrayItemNo in num:
-	print eachArrayItemNo
+	print eachArrayItemNo 
 	
 #1
 #100
 #1000
 #10000
-
-num = [1,"111+111",111+111,"one",[1,2,3]]
-for eachArrayItemNo in num:
-	print eachArrayItemNo
-	
-print num[0]
-print num[4][1]
-
-#1
-#111+111
-#222
-#one
-#[1,2,3]
-#1
-#2
