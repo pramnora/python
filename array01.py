7e553baf611cbca31f86896078ad8d171422f3c0
Paul Ramnora <paul@ramnora.com>
2015-03-24 16:07:55 +0000

    Create array01.py
    Exploring the use of Python 3 loops using: 'for varName in range()'

---
+++ array01.py
@@ -0,0 +1,23 @@
+for x in range(10): #the count starts at 0...and, finishes -1 from the loop specified count...eg. 0-9 
+    print (x), #the comma at the end prints horizontally across...with a space showing in between each number 
+              
+# 0 1 2 3 4 5  6 7 8 9
+
+for x in range(1,10): #this sets the count to start from 1...and, end at 9...= 10 (-1)
+    print (x),
+    
+#1 2 3 4 5 6 7 8 9
+
+for x in range(1,10,2): #this sets the step of the count...count up in steps of 2 at a time
+    print (x),
+    
+#1 3 5 7 9
+
+for x in range(0,10,2):
+	print (x) #no comma at the end prints out vertically
+    
+#0
+#2
+#4
+#6
+#8 
