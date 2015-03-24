7660a6b2071a513871fcd3e389a902ff79bb5f56
Paul Ramnora <paul@ramnora.com>
2015-03-24 16:20:51 +0000

    Update array01.py
    Python 3 arrays: Exploring the use of arrays

--- array01.py
+++ array01.py
@@ -1,23 +1,23 @@
-for x in range(10): #the count starts at 0...and, finishes -1 from the loop specified count...eg. 0-9 
-    print (x), #the comma at the end prints horizontally across...with a space showing in between each number 
-              
-# 0 1 2 3 4 5  6 7 8 9
+num = [1,10,100,1000,10000]
+for eachArrayItemNo in num:
+	print eachArrayItemNo
+	
+#1
+#100
+#1000
+#10000
 
-for x in range(1,10): #this sets the count to start from 1...and, end at 9...= 10 (-1)
-    print (x),
-    
-#1 2 3 4 5 6 7 8 9
+num = [1,"111+111",111+111,"one",[1,2,3]]
+for eachArrayItemNo in num:
+	print eachArrayItemNo
+	
+print num[0]
+print num[4][1]
 
-for x in range(1,10,2): #this sets the step of the count...count up in steps of 2 at a time
-    print (x),
-    
-#1 3 5 7 9
-
-for x in range(0,10,2):
-	print (x) #no comma at the end prints out vertically
-    
-#0
+#1
+#111+111
+#222
+#one
+#[1,2,3]
+#1
 #2
-#4
-#6
-#8 
