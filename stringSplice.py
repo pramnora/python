473293e3c497fe26e0cabd5367cff6be0b156c2f
Paul Ramnora <pramnora@yahoo.com>
2015-11-27 20:50:50 +0000

    Create stringSplice.py
    Using Python to splice strings.

---
+++ stringSplice.py
@@ -0,0 +1,28 @@
+# how to do Python string splicing...
+
+name = "Paul Ramnora"
+
+print(name)
+#Paul Ramnora
+
+# Example of how Python counts...
+# P a u l   R a m n o  r a
+# 0 1 2 3 4 5 6 7 8 9 10 11
+
+print(name[0])
+#P
+
+print(name[1:]
+#aul Ramnora
+
+print(name[0:4])
+#Paul
+
+print(name[-4])
+#n
+
+print(name[0:-4])
+#Paul Ram
+
+print(name[-8])
+#Paul
