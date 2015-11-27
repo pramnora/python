32c16bdbca8af5b5592e4022bfcda95f3a1ae2ed
Paul Ramnora <pramnora@yahoo.com>
2015-11-27 21:49:51 +0000

    Create array04.py
    Using Python number arrays with functions being applied.

---
+++ array04.py
@@ -0,0 +1,20 @@
+# How to use Python numeric arrays with functions...
+
+#assigning values into an array...
+numbers=[5,63,22,90,5]
+
+#printout out the array values...
+print(numbers)
+#5,63,22,90,5
+
+#finding what is the minimum number included inside of the array...
+print(min(numbers))
+5
+
+#finding what is the maximum number included inside of the array...
+print(max(numbers))
+90
+
+#finding out the number of times a list item repeats itself...
+print(numbers.count(5))
+#2 
