1e0122d447c60509c726ca55b8f31efea7d62c1d
Paul Ramnora <pramnora@yahoo.com>
2015-11-27 21:06:05 +0000

    Create stringPlaceHolders01.py
    Using Python with string/number placeholders.

---
+++ stringPlaceHolders01.py
@@ -0,0 +1,14 @@
+# Python's use of placeholders...
+
+# single string placeholder...
+print("%s, Paul" % ("Hi"))
+#Hi, Paul
+
+# multiple string placeholders...
+print("%s and %s ran up the hill" % ("Jack","Jill"))
+#Jack and Jill ran up the hill
+
+#both string and number placeholders...
+print("My name is, %s; and, I'm currently aged: %d." % ("Paul",14))
+#My name is, Paul; and, I'm currently aged: 14.
+
