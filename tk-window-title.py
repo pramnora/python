098fa4a92f7ff5088e289247015fd4a13e824fa1
Paul Ramnora <pramnora@yahoo.com>
2020-05-30 03:38:56 +0100

    Create tk-window-title.py
    Created 'new' file: tk-window-title.py...which places some title text inside of the Windows title bar.

---
+++ tk-window-title.py
@@ -0,0 +1,30 @@
+#----------------------------------------------------------------
+#   PROGRAM: Set window title heading text...
+#  LANGUAGE: Python 3.8.2/(python.org)
+#   LIBRARY: TKInter, used to build: GUI/Graphical User Interface
+#  COMPUTER: Home based PC/OS: Windows 10 Professional
+#    EDITOR: IDLE
+#----------------------------------------------------------------
+#    AUTHOR: Mr. Paul Ramnora
+#     EMAIL: pramnora@yahoo.com
+#  LOCATION: London, UK
+# COPYRIGHT: (c)2020-, Mr. Paul Ramnora./All rights reserved.
+#----------------------------------------------------------------
+#   CREATED: 300520 03:36 AM GMT
+#   UPDATED: 300520 03:36 AM GMT
+#----------------------------------------------------------------
+#      NOTE: This file places inside of an empty window...;
+#            some title text inside of the windows title bar.
+#----------------------------------------------------------------
+
+from tkinter import *
+
+root=Tk()       #...set root
+
+#...rest of code goes inside here..
+
+root.title("Title text goes here...")
+
+root.mainloop() #...execute main loop...
+
+
