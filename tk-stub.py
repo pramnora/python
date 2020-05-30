2e1262bae7c16e868c826e0b51346c88851c7451
Paul Ramnora <pramnora@yahoo.com>
2020-05-30 03:29:58 +0100

    Create tk-stub.py
    Created 'new' file: tk-stub.py...this file actually does nothing...but, to set up a main loop that's running...

---
+++ tk-stub.py
@@ -0,0 +1,31 @@
+#----------------------------------------------------------------
+#   PROGRAM: Set up main loop running...
+#  LANGUAGE: Python 3.8.2/(python.org)
+#   LIBRARY: TKInter, used to build: GUI/Graphical User Interface
+#  COMPUTER: Home based PC/OS: Windows 10 Professional
+#    EDITOR: IDLE
+#----------------------------------------------------------------
+#    AUTHOR: Mr. Paul Ramnora
+#     EMAIL: pramnora@yahoo.com
+#  LOCATION: London, UK
+# COPYRIGHT: (c)2020-, Mr. Paul Ramnora./All rights reserved.
+#----------------------------------------------------------------
+#   CREATED: 300520 03:16 AM GMT
+#   UPDATED: 300520 03:16 AM GMT
+#----------------------------------------------------------------
+#      NOTE: This file doesn't actually do anything...;
+#            other than to show a merely empty window;
+#            with 'tk' showing inside of the windows title bar.
+#            It represents the 'beginning' of your TKInter program;
+#            by setting up a main loop.
+#----------------------------------------------------------------
+
+from tkinter import *
+
+root=Tk() #...set root
+
+#...rest of code goes inside here..
+
+root.mainloop() #...execute main loop...
+
+
