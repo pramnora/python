bc56dae6c5793f450e40bfe18d7883fba32be418
Paul Ramnora <pramnora@yahoo.com>
2020-06-15 12:48:20 +0100

    Create cylinder-01.py
    Initial commit. Created file: [vp-cylinder-01.py] / This 2 line program allows one to create a 3D cylinder shape; which is both visible/and, also, draggable inside of your 'default' web browser software.

---
+++ library/vpython/cylinder-01.py
@@ -0,0 +1,18 @@
+#*** VPython 3D library
+
+#First, you have to install the VPython library; 
+#so, at the windows command line type...
+#pip install vpython
+#next, inside of IDLE type...
+
+from vpython import *
+cylinder()
+
+#...output...
+#when the code is run by saving it; and, then, pressing key: [F5]...
+#your 'default' web browser should, automatically, open; 
+#and, display a web page showing a 3D cylinder shape;
+#the shape can be clicked on using your 'right' mouse button/
+#and, by holding down the 'right' mouse button on top of the shape
+#you can also 'drag and drop' the shape all around the web browser screen
+#thus, placing the shape in multiple different positions.
