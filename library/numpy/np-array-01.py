70168e8c3a9dff1c4f9fbf7421eb0b79a2576c05
Paul Ramnora <pramnora@yahoo.com>
2020-06-15 07:28:16 +0100

    Update np-array-01.py
    Updated file commentary text description

--- library/numpy/np-array-01.py
+++ library/numpy/np-array-01.py
@@ -1,9 +1,20 @@
 import numpy as np
 xArrayName=np.arange(start=1,stop=10)
 xArrayName
-array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+#array([1, 2, 3, 4, 5, 6, 7, 8, 9])
 xArrayName.reshape(3,3)
-'...output...
-'array[[1, 2, 3],
-'      [4, 5, 6],
-'      [7, 8, 9]])
+xArrayName
+#...output...
+#array[[1, 2, 3],
+#      [4, 5, 6],
+#      [7, 8, 9]])
+
+#*** NOTE: When I tried saving the file as: [np-array-01.py];
+#          then, tried doing the printout by running the program;
+#          both printouts showed exactly the same 1 line output structure.
+
+#          However, when I moved over to the Python shell;
+#          and, there tried typing in the commands,
+#          it displayed a 3 x 3 grid as I was hoping to see.
+
+#          That is why the output commands here do have no: print()
