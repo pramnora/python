e4d05b62fc64d2e68719088df949eaff26a51d01
Paul Ramnora <pramnora@yahoo.com>
2020-06-15 07:12:51 +0100

    Create np-array-01.py
    Initial commit. Created file: [np-array-01.py] - The program draws a numpy array filled with numbers: 1-9/then, re-shapes it into becoming a 3x3 matrix grid

---
+++ library/numpy/np-array-01.py
@@ -0,0 +1,9 @@
+import numpy as np
+xArrayName=np.arange(start=1,stop=10)
+xArrayName
+array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+xArrayName.reshape(3,3)
+'...output...
+'array[[1, 2, 3],
+'      [4, 5, 6],
+'      [7, 8, 9]])
