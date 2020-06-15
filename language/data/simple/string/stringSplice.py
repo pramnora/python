b3cfde761d7dde959bad72ac1f9bf7f9c25405a1
Paul Ramnora <pramnora@yahoo.com>
2020-06-15 08:03:44 +0100

    Update stringSplice.py
    Updated file...added more printouts...to show exactly what is going on...including a name/numbers grid...as well as, individual printout statements

--- language/data/simple/string/stringSplice.py
+++ language/data/simple/string/stringSplice.py
@@ -2,27 +2,47 @@
 
 name = "Paul Ramnora"
 
+print("print(name)")
 print(name)
 #Paul Ramnora
 
 # Example of how Python counts...
-# P a u l   R a m n o  r a
-# 0 1 2 3 4 5 6 7 8 9 10 11
+print("P a u l   R a m n o  r a")
+print("0 1 2 3 4 5 6 7 8 9 10 11")
 
+print("\nprint(name[0]")
 print(name[0])
 #P
 
-print(name[1:]
-#aul Ramnora
+print("\nprint(name[1:]")
+print(name[1:])
+#paul Ramnora
 
+print("\nprint(name[0:4]")
 print(name[0:4])
 #Paul
 
+print("\nprint(name[-4]")
 print(name[-4])
 #n
 
+print("\nprint(name[-8] (an empty space character)")
+print(name[-8])
+#(empty space character)
+
+print("\nprint(name[0:-4]")
 print(name[0:-4])
 #Paul Ram
 
-print(name[-8])
+print("\nprint(name[:-8]")
+print(name[:-8])
 #Paul
+
+print("\nprint(name[:-1]")
+print(name[:-1])
+#Paul Ramnor
+
+
+print("\nprint(name[:]")
+print(name[:])
+#Paul Ramnora
