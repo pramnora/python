3456617b45ea604895480191142290aee46d08a0
Paul Ramnora <pramnora@yahoo.com>
2020-06-15 05:30:22 +0100

    Update sm-ascii-01.py
    Updated code to include code comment showing program output...

--- cryptography/common/ascii/sm-ascii-01.py
+++ cryptography/common/ascii/sm-ascii-01.py
@@ -1,3 +1,5 @@
-message="This is a secret message."
+message="This is a secret message"
 for eachChar in message:
   print(ord(eachChar.upper()),end="")
+#...output...
+# 847273833273833265328369678269843277698383657169
