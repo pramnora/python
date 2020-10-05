8b63cbd3e94242460c28f08a2bf18701978c40e4
Paul Ramnora <pramnora@yahoo.com>
2020-10-05 16:30:31 +0100

    Create main.py
    Created new file: [python/courses/pirple/python/assignments/variables/main.py]

---
+++ courses/pirple/python/assignments/variables/main.py
@@ -0,0 +1,31 @@
+"""
+         PROGRAM: Lesson 1/Assignment/Variables
+        LANGUAGE: Python/Version: 3.8.6
+-------------------------------------------------
+        COMPUTER: Home based Desktop PC
+OPERATING SYSTEM: Windows 10 Professional
+-------------------------------------------------
+          AUTHOR: Mr. Paul Ramnora
+        LOCATION: London, UK
+           EMAIL: pramnora@yahoo.com
+-------------------------------------------------
+          GITHUB: https://www.github.com/pramnora
+    RELATED CODE:
+-------------------------------------------------
+    FILE CREATED: 14:56 05/10/2020
+    LAST UPDATED: 14:56 05/10/2020
+"""
+
+"""
+     EXPLANATION:-
+                  The assignment asks to write code which illustrates...
+                  - the use of Python multi-line comments 
+                  - the use of Python single line comments
+                  - the use of variables in relation to describing one of your favourite songs
+                  - variable types should include: strings/integers/decimals(floats)
+                  - after assigning the variable values/you should also print the variables out
+                  - finally, zip up the file/and, upload it to be marked online 
+"""
+
+
+
