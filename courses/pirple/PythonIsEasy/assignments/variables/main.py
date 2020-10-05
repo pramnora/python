2ab81a95ac52933464e9d64f1012bf303735428d
Paul Ramnora <pramnora@yahoo.com>
2020-10-05 17:27:54 +0100

    Update main.py
    Minor edits: Added some extra underlining/-etc.

--- courses/pirple/PythonIsEasy/assignments/variables/main.py
+++ courses/pirple/PythonIsEasy/assignments/variables/main.py
@@ -1,17 +1,17 @@
 """
-         PROGRAM: Lesson 1/Assignment/Variables
+         PROGRAM: Lesson 1/Assignment: Variables/Comments
         LANGUAGE: Python/Version: 3.8.6
--------------------------------------------------
+---------------------------------------------------------
         COMPUTER: Home based Desktop PC
 OPERATING SYSTEM: Windows 10 Professional
--------------------------------------------------
+---------------------------------------------------------
           AUTHOR: Mr. Paul Ramnora
         LOCATION: London, UK
            EMAIL: pramnora@yahoo.com
--------------------------------------------------
+---------------------------------------------------------
           GITHUB: https://www.github.com/pramnora
     RELATED CODE: https://github.com/pramnora/python/blob/master/courses/pirple/PythonIsEasy/assignments/variables/main.py
--------------------------------------------------
+---------------------------------------------------------
     FILE CREATED: 14:56 05/10/2020
     LAST UPDATED: 14:56 05/10/2020
 """
