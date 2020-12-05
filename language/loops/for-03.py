0a8958fe5b0b3832cabca6207defbb8370ea906a
Paul Ramnora <pramnora@yahoo.com>
2020-12-05 15:28:18 +0000

    Create for-03.py
    Created file: [for-03.py] - This uses a rather more complex version of the for loop statement: with an expression at the start/followed by the for statements afterwards...

---
+++ language/loops/for-03.py
@@ -0,0 +1,22 @@
+# -----------------------------------------------------
+# File created: 15:22 05/12/2020
+# Last updated: 15:22 05/12/2020
+# -----------------------------------------------------
+# Original code borrowed from...
+# YouTube: Channel: freeCodeCamp.org 
+# Intermediate Python Programming Course
+# https://www.youtube.com/watch?v=HGOBQPFzWKo&t=7222s
+# video point: 2:00:31/5:55:46
+# -----------------------------------------------------
+# Original code further adapted by me:
+# (c) 2020-, Mr. Paul Ramnora./All rights reserved.
+# -(The original code was written out much longer...; 
+#  I decided to shorten it down to just 'one' line.)-
+# -----------------------------------------------------
+
+# SYNTAX Usage: 
+# [expression for n in [n,n,...]] 
+
+# Example 1: For Loop...
+print([x*2 for x in [1,2,3,4,5]])
+# outputs: [2,4,6,8,10]
