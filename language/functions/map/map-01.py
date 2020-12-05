fda02c2eca3521aac41ad5cbd6d236e85e7f3179
Paul Ramnora <pramnora@yahoo.com>
2020-12-05 15:53:53 +0000

    Create map-01.py
    Created file: [map-01.py] - Shows how to use the 'map' function...which takes as its arguments both a: map(function, sequence)

---
+++ language/functions/map/map-01.py
@@ -0,0 +1,18 @@
+# -----------------------------------------------------
+# File created: 15:47 05/12/2020
+# Last updated: 15:47 05/12/2020
+# -----------------------------------------------------
+# Original code borrowed from...
+# YouTube: Channel: freeCodeCamp.org 
+# Intermediate Python Programming Course
+# https://www.youtube.com/watch?v=HGOBQPFzWKo&t=7222s
+# video point: 2:00:31/5:55:46
+# -----------------------------------------------------
+# Original code further adapted by me: 
+# (c) 2020-, Mr. Paul Ramnora./All rights reserved.
+# -----------------------------------------------------
+
+# SYNTAX Usage
+# keyword: map(func, seq)
+print(list(map(lambda x:x*2,[1,2,3,4,5])))
+# outputs: [2,4,6,8,10]
