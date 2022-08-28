ac8c6c9d754ab505fec8a03d997f352676e3334a
Paul Ramnora <pramnora@yahoo.com>
2022-08-28 15:09:16 +0100

    Update import01a.py
    Extended commentary notes...

--- language/modules/own/01/import01a.py
+++ language/modules/own/01/import01a.py
@@ -1,8 +1,14 @@
+# Make calls to an external file: [myModules.py];
+# so, that one can use modules written inside of that external file inside of this same file.
+
+# NOTE: Using this syntax means...one has to prefix the external module file name: myModules...;
+#       before one can call the named function: two()/three()...written inside of that external file.
+
 import myModules
 
 def one():
 	print("Function one")
 
-one()         # calling an *internal* module function code
+one()             # calling an *internal* module function code
 myModules.two()   # calling an *external* module function code
 myModules.three() # calling an *external* module function code
