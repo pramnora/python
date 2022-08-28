bec57aa7a89c3ac7ae9ec13e068549d8a0e470ca
Paul Ramnora <pramnora@yahoo.com>
2022-08-28 22:30:45 +0100

    Update runDOS.bat
    Deleted REM lines...plus, call to: python callMyMathsLibrary.py (because, when I tried testing if it works on my system...it didn't work!

--- language/modules/own/02/runDOS.bat
+++ language/modules/own/02/runDOS.bat
@@ -1,6 +1,3 @@
 @ECHO OFF
 CMD
-Python callMyMathsLibrary.py
-REM *** You will need to have Python programming language already installed on your Windows operating system for this to work;   
-REM     Python programming language can be downloaded for FREE from: https://www.python.org  
 PAUSE
