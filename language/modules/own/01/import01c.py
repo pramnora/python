1d3e5c4394ce45f2498ac1da0cdf2e815ba2f79b
Paul Ramnora <pramnora@yahoo.com>
2022-08-28 15:25:01 +0100

    Update import01c.py
    Re-wrote descriptive comments to read more clear

--- language/modules/own/01/import01c.py
+++ language/modules/own/01/import01c.py
@@ -2,9 +2,9 @@
 # so, that one can use modules written inside of that external file inside of this same file.
 
 # NOTE: Using this syntax means...
-#       one doesn't need to include the 'myModules' name, anymore...; 
-#       and, therefore, you don't need to prefix an external function name before calling it;
-#       but, instead, you can simply call using the function name: two()/three();
+#       imports all (*) functions from the external file called: 'myModules.py';
+#       furthermore, you don't need to prefix any external function name before calling each external function;
+#       but, instead, you can quite simply call using the function name: two()/three();
 #       as if you were calling an internally written function.
 
 from myModules import *
