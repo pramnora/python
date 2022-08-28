22170801a4c38aca90bfee5d091b7ae70444eddd
Paul Ramnora <pramnora@yahoo.com>
2022-08-28 20:53:08 +0100

    Create callMyMathsLibrary.py
    Added file: [callMyMathsLibrary.py] Demonstrates how to make calls to an external library module.

---
+++ language/modules/own/02/callMyMathsLibrary.py
@@ -0,0 +1,15 @@
+# Call external file modules...
+
+from myMathsLibrary import *
+
+# variable definitions...
+
+num1 = 3
+num2 = 2
+
+# main program...
+
+print(num1," + ",num2," = ",add(num1,num2))      # 3
+print(num1," - ",num2," = ",subtract(num1,num2)) # 1
+print(num1," * ",num2," = ",multiply(num1,num2)) # 6
+print(num1," / ",num2," = ",divide(num1,num2))   # 1.5
