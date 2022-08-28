374c2e96f39b3a2da2069884999f4dd2691c2efc
Paul Ramnora <pramnora@yahoo.com>
2022-08-28 15:56:54 +0100

    Update import01d.py
    Re-wrote code comments to read a lot more clear

--- language/modules/own/01/import01d.py
+++ language/modules/own/01/import01d.py
@@ -2,15 +2,14 @@
 # so, that one can use a single 'specific' named module which is written inside of that external file...inside of this same file.
 
 # NOTE: Using this syntax means...
-#       one can shorten the long name: myModules...to become, instead, quite simply: mm
-#       one needs to prefix the external module file name(in this case shortened): mm...;
-#       before one can call the named external function to use it: mm.two()/mm.three()
+#       one can specify which of the named functions inside of external file: myModules, is to be called;
+#       thus, helping to save memory...by not calling all (*).
 
 from myModules import two
 
 def one():
 	print("Function one")
 
-one()      # calling an *internal* module function code
-mm.two()   # calling an *external* module function code
-mm.three() # calling an *external* module function code/this call will bug out...as function three hasn't been declared...?!
+one()   # calling an *internal* module function code
+two()   # calling an *external* module function code
+three() # calling an *external* module function code/this call will bug out...as function three hasn't been declared...?! (NameError: name 'three' is not defined)
