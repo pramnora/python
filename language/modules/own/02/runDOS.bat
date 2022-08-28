0c8b9be0b125361869c10b9cb4a5915e18ee0518
Paul Ramnora <pramnora@yahoo.com>
2022-08-28 22:07:26 +0100

    Update runDOS.bat
    Added instructions of where to download Python programming language from

--- language/modules/own/02/runDOS.bat
+++ language/modules/own/02/runDOS.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
 CMD
 Python callMyMathsLibrary.py
+REM *** You will need to have Python language already installed on your Windows operating system for this to work;   
+REM     Python programming language can be downloaded for FREE from: https://www.python.org  
 PAUSE
