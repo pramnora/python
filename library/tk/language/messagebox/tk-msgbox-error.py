7c42fb1cd4f29d59323eb589f7d50db9afc1a3fd
Paul Ramnora <pramnora@yahoo.com>
2022-09-06 12:08:27 +0100

    Update tk-msgbox-error.py
    Edited window output to say: "This is an error message box."

--- library/tk/language/messagebox/tk-msgbox-error.py
+++ library/tk/language/messagebox/tk-msgbox-error.py
@@ -4,4 +4,4 @@ from tkinter import messagebox
 root = Tk()
 root.withdraw()
 
-messagebox.showerror("Tkinter Window","Hello World")
+messagebox.showerror("Tkinter Window","This is an error message box")
