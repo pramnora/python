6d71de7e10e8e9ce63f9c7504773fece492d9d51
Paul Ramnora <pramnora@yahoo.com>
2023-02-13 11:21:31 +0000

    Create filing03.py
    Created file: [filing03.py] Demonstrates how to using a multi-line list array to write text into a file...by using the 'fileHandle.writelines(myListName)' method.

---
+++ language/filing/filing03.py
@@ -0,0 +1,41 @@
+# Created: Mon 13th Feb 2023 11:15 AM GMT
+# Updated: Mon 13th Feb 2023 11:15 AM GMT
+
+# fh1 = fileHandle1
+
+# open file/and, write a single line of data into it...
+
+fh1=open ("test02.txt",'w')
+fh1.write("abc\n")
+fh1.close
+
+# open file/and, read from it...;
+# by displaying it's contents onto the output screen...
+
+fh1=open ("test02.txt",'r')
+print(fh1.read())
+fh1.close
+
+# create a list based array...;
+# to hold multiple lines of data...
+
+myData=[
+ "def\n"
+ "ghi\n"
+ "jkl\n"
+]
+
+# open file/in order to append to it...;
+# without overwriting any of the file's previous contents...
+
+fh1=open ("test02.txt",'a')
+fh1.writelines(myData)
+fh1.close
+
+# open file/and, read from it...;
+# by displaying it's contents onto the output screen...
+
+fh1=open ("test02.txt",'r')
+print(fh1.read())
+fh1.close
+
