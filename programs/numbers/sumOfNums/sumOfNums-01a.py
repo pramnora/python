55ba5293f9282904678486a3c0851fc4c93f7bc4
Paul Ramnora <pramnora@yahoo.com>
2023-02-13 15:07:13 +0000

    Update sumOfNums-01a.py
    Minor edit: Re-wrote the number 1,000,000 to go down below/instead, of up above.

--- programs/numbers/sumOfNums/sumOfNums-01a.py
+++ programs/numbers/sumOfNums/sumOfNums-01a.py
@@ -15,11 +15,11 @@
 # ----------------------------------------
 
 # This program uses a for loop to add up each successive number
-# going from 1...up to...1 million/(1,000,000).
+# going from 1...up to...1 million.
 
 totalNums=0      # total starts at 0 
 maxNum=1000000   # 1 million digits to be added up/
-                 # (1 million = a 1 followed by 6 x 0's)
+                 # (1 million = a 1 followed by 6 x 0's : 1,000,000)
 
 for eachNum in range(1,maxNum+1):
     totalNums += eachNum
