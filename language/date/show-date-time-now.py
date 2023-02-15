193329a5e85298a393203594910196b84c40ea56
Paul Ramnora <pramnora@yahoo.com>
2023-02-15 12:27:37 +0000

    Create show-date-time-now.py
    Added file: [show-date-time-now.py] Shows the current date/time.

---
+++ language/date/show-date-time-now.py
@@ -0,0 +1,8 @@
+# This code was borrowed from...
+# https://pyscript.net/examples/hello_world.html
+# Created: 12:22 15/02/2023
+# Updated: 12:22 15/02/2023
+
+from datetime import datetime
+now = datetime.now()
+print(now.strftime("%d/%m/%Y, %H:%M:%S"))
