0f5bb2fc0a925d2fcd1cb23eb8b961d0d3db9caf
Paul Ramnora <pramnora@yahoo.com>
2023-07-19 04:38:51 +0100

    Create lambda-02.py
    Created file: [lambda-02.py] Copied an YouTube example of how to use the one line 'lambda' function

---
+++ language/functions/lambda/lambda-02.py
@@ -0,0 +1,11 @@
+'''
+This code borrowed from YouTube...
+https://www.youtube.com/shorts/ss-I6WAiMFA
+
+Created: 03:07 19/07/2023
+Updated: 03:07 19/07/2023
+'''
+print((lambda x,y:x+y)(1,2))
+
+# output...
+# 3
