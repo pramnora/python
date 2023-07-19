9bf7c26413359105667cbc57738061a5de13ce09
Paul Ramnora <pramnora@yahoo.com>
2023-07-19 03:13:25 +0100

    Create list-comprehension-01.py
    Created file: [list-comprehension-01.py] shows how to use one line to create a list

---
+++ language/data/complex/list/list-comprehension-01.py
@@ -0,0 +1,20 @@
+'''
+This code borrowed from YouTube...
+https://www.youtube.com/shorts/QdMzVy2GiKc
+
+Created: 03:04 19/07/2023
+Updated: 03:05 19/07/2023
+'''
+
+names = [
+    "Charles",
+    "David",
+    "Paul"   
+]
+
+# list comprehension...
+
+length = [len(name) for name in names]
+
+print(length)
+
