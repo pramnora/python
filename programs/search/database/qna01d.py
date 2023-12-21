0268107e3c3709ba5a26299cb9f6d21ab0e262a9
Paul Ramnora <pramnora@yahoo.com>
2023-12-21 13:54:46 +0000

    Create qna01d.py
    Created file: [qna01d.py]...version 4 of [qna01a.py]
    
    This version includes a quit option when the user types 'q/Q'

---
+++ programs/search/database/qna01d.py
@@ -0,0 +1,49 @@
+# -----------------------------------
+#  PROGRAM: Question & Answer test...
+#  VERSION: 1.0.4
+# LANGUAGE: Python 3
+#       OS: Linux Mint
+# COMPUTER: Home based PC 
+# -----------------------------------
+# Created: Wed 201223 07:32 AM GMT
+# Updated: Thu 211223 13:21 PM GMT
+# -----------------------------------
+
+# WHAT THE PROGRAM DOES
+# This is a very simple 
+# 'qna/question and answer' example database...;
+# which can very quickly and easily be adapted
+# to suit many different types of 'question/answer' tests.
+
+# ------------------------
+# Variable declarations...
+# ------------------------
+
+# First, create 2 parallel arrays...
+# q, stores the questions
+# a, stores the answers
+
+q=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+a=["alpha","bravo","charlie","delta","echo","foxtrot","golf","hotel","india","juliet","kilo","lima","mike","november","oscar","papa","quebec","romeo","sierra","tango","uniform","victor","whisky","x-ray","yankee","zulu"]
+
+n=0  # variable stores the current answer counter number
+g="" # variable stores the users current guess
+s=0  # variable stores what is the users current score
+
+# ---------------
+# Main program...
+# ---------------
+
+for x in q:
+    print(n+1,"> What is",x,"?")
+    g=input("Enter your guess/('q'=quit): ")
+    if (g.lower()=="q"):
+        break
+    else:
+        print("   The answer is:",a[n])
+        if g==a[n]: 
+           s+=1
+        n+=1
+        print()
+print("-Finished!")
+print("Your total score is: ",s, "out of",n,"correct guesses.")
