716f47a32a016fec0679bdebcaa3a54146870364
Paul Ramnora <pramnora@yahoo.com>
2023-12-21 14:23:17 +0000

    Update qna01-min.py
    made a mistake...corrected it: changed ' to #

--- programs/search/database/qna01-min.py
+++ programs/search/database/qna01-min.py
@@ -1,4 +1,4 @@
-'*** QUIZ: NATO Phonetic Codes
+#*** QUIZ: NATO Phonetic Codes
 q=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 a=["alpha","bravo","charlie","delta","echo","foxtrot","golf","hotel","india","juliet","kilo","lima","mike","november","oscar","papa","quebec","romeo","sierra","tango","uniform","victor","whisky","x-ray","yankee","zulu"]
 n,s,g=0,0,"" 
