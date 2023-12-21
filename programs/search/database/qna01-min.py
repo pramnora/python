dd40cde6efef4662bc87832ecaa05b5f3f979b37
Paul Ramnora <pramnora@yahoo.com>
2023-12-21 14:22:41 +0000

    Update and rename qna01d.py to qna01-min.py
    This is the minimized version of the program, version 1

---
+++ programs/search/database/qna01-min.py
@@ -0,0 +1,17 @@
+'*** QUIZ: NATO Phonetic Codes
+q=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+a=["alpha","bravo","charlie","delta","echo","foxtrot","golf","hotel","india","juliet","kilo","lima","mike","november","oscar","papa","quebec","romeo","sierra","tango","uniform","victor","whisky","x-ray","yankee","zulu"]
+n,s,g=0,0,"" 
+for x in q:
+    print(n+1,"> What is",x,"?")
+    g=input("Enter your guess/('q'=quit): ")
+    if (g.lower()=="q"):
+        break
+    else:
+        print("   The answer is:",a[n])
+        if g==a[n]: 
+           s+=1
+        n+=1
+        print()
+print("-Finished!")
+print("Your total score is: ",s, "out of",n,"correct guesses.")
