6498d3fe2dc2fc6eba1556eef83c91d99d12ea62
Paul Ramnora <pramnora@yahoo.com>
2023-12-21 14:46:14 +0000

    Update and rename qna01-min.py to qna01a-min.py
    Updated file: [qna01a-min.py] This is the minimized version of the code but using long variable names

---
+++ programs/search/database/qna01a-min.py
@@ -0,0 +1,17 @@
+#*** QnA/Question and Answer test QUIZ: NATO Phonetic Codes/LANGUAGE: Python
+strQuestions=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+strAnswers=["alpha","bravo","charlie","delta","echo","foxtrot","golf","hotel","india","juliet","kilo","lima","mike","november","oscar","papa","quebec","romeo","sierra","tango","uniform","victor","whisky","x-ray","yankee","zulu"]
+intCounterNo,intScoreNo,strGuess=0,0,"" 
+for strEachQuestion in strQuestions:
+    print(intCounterNo+1,"> What is",strEachQuestion,"?")
+    strGuess=input("Enter your guess/('q'=quit): ")
+    if (strGuess.lower()=="q"):
+        break
+    else:
+        print("   The answer is:",strAnswers[intCounterNo])
+        if strGuess==strAnswers[intCounterNo]: 
+           intScoreNo+=1
+        intCounterNo+=1
+        print()
+print("-Finished!")
+print("Your total score is: ",intScoreNo, "out of",intCounterNo,"correct guesses.")
