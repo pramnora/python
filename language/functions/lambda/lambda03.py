963941d92b2f03ff4a2787e5b9fc920c9e67fd43
Paul Ramnora <pramnora@yahoo.com>
2024-01-25 01:04:12 +0000

    Create lambda03.py
    Initial commit: [lambda01.py] Demonstrates 3 different uses of lambda...

---
+++ language/functions/lambda/lambda03.py
@@ -0,0 +1,26 @@
+  GNU nano 6.2                       lambda01.py                                
+# Created: Wed 240124 12:38 PM GMT
+# Updated: Wed 240124 12:38 PM GMT
+
+# Example 1/Anonymous lambda function...
+
+print((lambda x,y:x+y)(1,2))
+
+# Example 2/Named lambda function
+
+add = lambda x,y:x+y
+print(add(3,4))
+
+# Example 3/Lambda with if-else
+
+findLargestNo = lambda x,y:x if x>y else y
+print(findLargestNo(10,5))
+
+
+
+
+
+
+
+^G Help      ^O Write Out ^W Where Is  ^K Cut       ^T Execute   ^C Location
+^X Exit      ^R Read File ^\ Replace   ^U Paste     ^J Justify   ^/ Go To Line
