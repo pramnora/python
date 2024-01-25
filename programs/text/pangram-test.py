d2b4a841a72fa1b3b32bed56086f80caa32ae8e8
Paul Ramnora <pramnora@yahoo.com>
2024-01-25 04:18:00 +0000

    Update pangram-test.py
    Updated file: [pangram-test.py] Chained functions: Remove spaces/remove full stop/change upper case to lower

--- programs/text/pangram-test.py
+++ programs/text/pangram-test.py
@@ -13,14 +13,16 @@
 # inside of the pangram: 'the quick brown fox jumps over the lazy dog'     
 
 # Initialise variables...
-pangram="the quick brown fox jumps over the lazy dog"
+pangram="The quick brown fox jumps over the lazy dog."
 alphabet="abcdefghijklmnopqrstuvwxyz"
 
 # Remove all spaces from pangram text...
-pangram=pangram.replace(" ","")
+# Remove the full stop: (.)
+# Change all upper case letters into being lower case, instead...
+pangram=pangram.replace(" ","").replace(".","").lower()
 
 # print pangram...
-print(" pangram =",pangram)
+print("pangram =",pangram)
 
 # print pangram length...
 print("pangram length =",len(pangram))
@@ -32,18 +34,10 @@ print("alphabet =",alphabet)
 print("alphabet length = ",len(alphabet))
 
 # check if each letter of the alphabet is contained inside of the pangram...
-# by using a for loop.../together with an if/else test...
+# by using a for loop...
 for x in alphabet:
         if x in pangram:
                 print("found letter:", x," in ", pangram)
         else:
                 print("letter:",x,"NOT found in ", pangram)
 
-
- 
-
-
-
-                               [ Read 33 lines ]
-^G Help      ^O Write Out ^W Where Is  ^K Cut       ^T Execute   ^C Location
-^X Exit      ^R Read File ^\ Replace   ^U Paste     ^J Justify   ^/ Go To Line
