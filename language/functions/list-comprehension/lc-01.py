e18b6c34875cc6189220c11d087a0eacff2d2cd7
Paul Ramnora <pramnora@yahoo.com>
2024-01-25 07:53:18 +0000

    Create lc-01.py
    Created file: [lc-01.py] Uses 'list comprehension' technique to fill a list with numbers, 1 up to 10.

---
+++ language/functions/list-comprehension/lc-01.py
@@ -0,0 +1,12 @@
+# Created: Thu 250124 04:50 PM GMT
+# Updated: Thu 250124 04:50 PM GMT
+
+# Python, List Comprehension...;
+# fill list with numbers by using a for statement...
+
+list = [n for n in range(1,11)]
+
+# print list...
+
+print(list) # [1,2,3,4,5,6,7,8,9,10]
+
