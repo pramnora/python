ad99e68cec69da40cf7a565bc1388abf2c39abbf
Paul Ramnora <pramnora@yahoo.com>
2024-08-14 18:41:53 +0100

    Create ascii-encoder-01.py
    Initial commit. [ascii-encoder-01.py] This program simply changes alphabet letters: A-Z...into being alphabet numbers: 1-26...; according to whichever 'secret message' was typed in.

---
+++ programs/encryption/my_code/ascii-encoder-01.py
@@ -0,0 +1,22 @@
+# File created: Wed 14 Aug 2024 17:21 PM GMT 
+# Last updated: Wed 14 Aug 2024 17:21 PM GMT
+# ------------------------------------------
+# This is the start of an encoding program...
+# It tranlates a plain text alphabet message: A-Z;
+# into becoming a series of alphabet numbers: 1-26.
+# ------------------------------------------
+
+# initialise variable...
+secret_message="This is a secret message."
+
+# shorten variable name/plus, change each letter to become upper case...
+sm=secret_message.upper()
+
+for eachChar in sm: # loop through each character in secret message...
+    if eachChar >= "A" and eachChar <= "Z": # check character is: A-Z 
+        n = ord(eachChar)-64 # get letter ASCII number: 65-90/deduct 64: (1-20)
+        if n < 10: # if ASCII number is less than 10
+            n  = '0' + (str(n)) # add a 0 prefix digit to that number
+        print(n,end="") # print numbers all on one single same line
+ 
+print("\n") # vertical line break/ceases all output to one same line
