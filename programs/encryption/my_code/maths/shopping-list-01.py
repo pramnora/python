2c62f893c713d60f3f417338b31e95ad35b715eb
Paul Ramnora <pramnora@yahoo.com>
2024-08-16 23:54:09 +0100

    Create shopping-list-01.py
    Initial commit. Added file; [shopping-list-01.py] The program aims to calculate the sum of each shopping list item...which is stored inside of an array/then, outputs what is the total sum result.

---
+++ programs/encryption/my_code/maths/shopping-list-01.py
@@ -0,0 +1,39 @@
+# ---------------------------------------
+#  PROGRAM: Maths: Shopping list calulator
+# LANGUAGE: Python3
+# ---------------------------------------
+# COMPUTER: Home based PC
+#       OS: Linux Mint
+# ---------------------------------------
+#   AUTHOR: Mr. Paul Ramnora
+#    EMAIL: paulramnoracoder@yahoo.com
+# ---------------------------------------
+#  CREATED: Fri 16 Aug 2024 16:53 PM GMT
+#  UPDATED: Fri 16 Aug 2024 16:53 PM GMT
+# ---------------------------------------
+
+# variable declarations...
+bananas, oranges, tomatoes = 7 * 0.29, 1.50, 1.10     # initialising mutliple variables using just one same line
+
+# array declaration...
+araShopping = [bananas, oranges, tomatoes]            # create an array, and, initialise it using variable values
+
+# print out each of the array values...
+for eachItem in araShopping:                          # use loop to go through each array value
+    print(eachItem)                                   # print out each array value     
+    
+# calculate and print shopping total value...     
+sumTotal = 0                                          # initialise sumTotal variable
+for each_item in araShopping:                         # use for loop to go through each array value
+    sumTotal += each_item                             # calculate sum total value 
+print("====")                                         # print equals line    
+print(sumTotal)                                       # printout sum total value   
+
+# ---------------------
+# Output...
+# 
+# 2.03
+# 1.5
+# 1.1
+# ====
+# 4.63
