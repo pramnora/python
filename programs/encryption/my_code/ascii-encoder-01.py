e91b4619765bcadfd3dd2794512d40e14fd80324
Paul Ramnora <pramnora@yahoo.com>
2024-08-16 23:45:00 +0100

    Update ascii-encoder-01.py
    Decided to line up each 'inline comment'...to make the program easier to read/understand.

--- programs/encryption/my_code/ascii-encoder-01.py
+++ programs/encryption/my_code/ascii-encoder-01.py
@@ -12,14 +12,14 @@ secret_message="This is a secret message."
 # shorten variable name/plus, change each letter to become upper case...
 sm=secret_message.upper()
 
-for eachChar in sm: # loop through each character in secret message...
+for eachChar in sm:                         # loop through each character in secret message...
     if eachChar >= "A" and eachChar <= "Z": # check character is: A-Z 
-        n = ord(eachChar)-64 # get letter ASCII number: 65-90/deduct 64: (1-26)
-        if n < 10: # if ASCII number is less than 10
-            n  = '0' + (str(n)) # add a 0 prefix digit to that number
-        print(n,end="") # print numbers all on one single same line
+        n = ord(eachChar)-64                # get letter ASCII number: 65-90/deduct 64: (1-26)
+        if n < 10:                          # if ASCII number is less than 10
+            n  = '0' + (str(n))             # add a 0 prefix digit to that number
+        print(n,end="")                     # print numbers all on one single same line
  
-print("\n") # vertical line break/ceases all output to one same line
+print("\n")                                 # vertical line break/ceases all output to one same line
 
 # -----------------------------------------------------------------------
 # The secret message gets translated into being a series of number pairs:   
