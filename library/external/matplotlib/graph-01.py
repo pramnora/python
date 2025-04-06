06c783dd371e5ad77e170d4506c3c3fac209a44a
Paul Ramnora <pramnora@yahoo.com>
2025-04-06 17:53:05 +0100

    Update graph-01.py
    Minor edits...to comments code

--- library/external/matplotlib/graph-01.py
+++ library/external/matplotlib/graph-01.py
@@ -1,10 +1,11 @@
 # Created: 060424 14:03 PM GMT
 # Updated: 060424 14:03 PM GMT
+
 # Example code borrowed from Simplilearn Python3 tutorial.
 # https://www.simplilearn.com
 
 # The following 3 lines of Python3 code 
-# can be used to create a very simple graph; 
+# can be used to create a very simple diagional straight line graph:(/); 
 # which is, then, being displayed on screen.
 
 # ------------------------------------------------
