63314b7bf72524a7caf7b09b6bf6139bae02f577
Paul Ramnora <pramnora@yahoo.com>
2025-04-06 14:05:54 +0100

    Create graph-01.py
    Use of library: matplotlib...to create a simple graph/and, display it on screen..with not more than just merely 3 lines of code

---
+++ library/external/matplotlib/graph-01.py
@@ -0,0 +1,13 @@
+# Created: 060424 14:03 PM GMT
+# Updated: 060424 14:03 PM GMT
+# Example code borrowed from Simplilearn tutorial.
+
+# The following 3 lines of Python3 code 
+# can be used to create a very simple graph; 
+# which is, then, being displayed on screen.
+
+# ------------------------------------------------
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4],[1,2,3,4])
+plt.show()
