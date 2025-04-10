0b984074fbfc506ed88e1de1587c5c41bfd8a492
Paul Ramnora <pramnora@yahoo.com>
2025-04-10 12:42:27 +0100

    Update and rename test.py to shapes.py
    Renamed file: [test.py] -> [shapes.py]
    Also, added code with comments...;
    this program can draw 'any' shape.

---
+++ library/turtle/shapes/shapes.py
@@ -0,0 +1,20 @@
+import turtle
+
+'''
+ This program allows you to create 'any' shape...;
+ in order to use it, effectively, 
+ then, you just have to call the function: drawShape(arg1,arg2) using 2 arguments:
+ - length, how long you wish the shape to be: small/medium/big
+ - noOfSides, each side the shape has (eg. a triangle has 3 sides/a square 4 sides/and, so on...)
+  ...after that is done; the program will calculate the rest for you: (360/noOfSides);
+  as well as, draw the shape on screen so that you can view that shape.
+'''
+
+def drawShape(length,noOfSides):
+    for drawEachSide in range(1,noOfSides+1):
+        turtle.forward(length)
+        turtle.left(360/noOfSides)
+
+drawShape(100,3) # draws a triangle
+drawShape(100,4) # draws a square 
+drawShape(100,5) # draws a pentagon
