0304166b570669a815f652ed13592eedbfdc1e42
Paul Ramnora <pramnora@yahoo.com>
2025-04-13 12:53:23 +0100

    Update data-info.py
    Added a comment about: initialize...

--- language/general/data-info.py
+++ language/general/data-info.py
@@ -1,4 +1,4 @@
-x = 1
-print(x) # print variable value
+x = 1          # initialize variable by giving it a value
+print(x)       # print variable value
 print(type(x)) # print data type of variable
-print(id(x)) # print memory address of variable
+print(id(x))   # print memory address of variable
