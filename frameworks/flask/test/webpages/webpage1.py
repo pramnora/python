f392af05ccae4bce2dc484716ac2bc34e0f9d716
Paul Ramnora <pramnora@yahoo.com>
2025-04-20 01:30:34 +0100

    Update webpage1.py
    Creating a first homepage...

--- frameworks/flask/test/webpages/webpage1.py
+++ frameworks/flask/test/webpages/webpage1.py
@@ -1,1 +1,23 @@
-test
+from flask import Flask
+
+app = Flask(__name__)
+
+homepage='''
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <title>Homepage</title>
+ </head>
+ <body>
+  <p>Welcome to my Flask homepage! ;-)</p>
+ </body>
+</html>
+'''
+
+@app.route('/')
+def home():
+    return homepage
+
+if __name__ == '__main__':
+    app.run(debug=True)
