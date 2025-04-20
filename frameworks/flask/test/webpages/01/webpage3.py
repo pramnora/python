d56724ad61dc8703d781320f2c118d8947e76a1a
Paul Ramnora <pramnora@yahoo.com>
2025-04-20 01:42:53 +0100

    Update and rename frameworks/flask/test/webpages/webpage3.py to frameworks/flask/test/webpages/01/webpage3.py
    Created file: [webpage3.py] Displays 3 x separate HTML web page files: index.html/page2.html/page3.html.

---
+++ frameworks/flask/test/webpages/01/webpage3.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+# called using: http://127.0.0.1:5000
+
+@app.route('/page2')
+def pageNo2():
+    return render_template("page2.html")
+# called using: http://127.0.0.1:5000/page2
+
+@app.route('/pages/page3')
+def pageNo3():
+    return render_template("page3.html")
+# called using: http://127.0.0.1:5000/pages/page3
+
+if __name__ == '__main__':
+    app.run(debug=True)  test
