ee83c6330003213d39777fe9067a05fd4d10301c
Paul Ramnora <pramnora@yahoo.com>
2025-04-24 21:45:29 +0100

    Update dataclass1.py
    Added some extra printout statements of each var.../together with line comments stating what the printout displays.

--- language/class/dataclass/dataclass1.py
+++ language/class/dataclass/dataclass1.py
@@ -8,3 +8,8 @@ class Class_Name:
 
 test_case:Class_Name=Class_Name("Anystring",0,['a','b','c'])
 print(test_case)
+print("var1: ",test_case.var1)       # prints: Anystring
+print("var2: ",test_case.var2)       # prints: 0
+print("var3: ",test_case.var3)       # prints: ['a','b','c']
+print("var3[0]: ",test_case.var3[0]) # prints: a
+
