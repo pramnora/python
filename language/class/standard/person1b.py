71f6e967ca58c834a48d8c5dfaea2fac69d577f9
Paul Ramnora <pramnora@yahoo.com>
2025-04-24 22:52:26 +0100

    Create person1b.py
    My own adaptation of: [Person1.py] code...using short form. Didn't explicitly declare variables types: str,int,list[str]

---
+++ language/class/standard/person1b.py
@@ -0,0 +1,18 @@
+# --------------------------------------------------------------- 
+# CREATED: Thu 240425 19:15 PM GMT
+# UPDATED: Thu 240425 19:15 PM GMT
+# --------------------------------------------------------------- 
+# Borrowed this code from...
+# 5 Cool Dataclass Features In Python - YouTube channel: Indently
+# https://www.youtube.com/watch?v=E9tl3hP03lk
+# --------------------------------------------------------------- 
+# This is my own adaptation of the code...; using a short form...
+
+class Person:
+    def __init__(self, name, age, friends):
+        self.name=name
+        self.age=age
+        self.friends=friends
+bob: Person=Person(name='testing',age=29,friends=['Luigi','James'])
+print(bob)
+print(bob.name,bob.age,bob.friends)
