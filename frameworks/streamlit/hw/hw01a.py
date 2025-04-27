bae9b97cb04cd09c44a53839d9e41bd5807d9175
Paul Ramnora <pramnora@yahoo.com>
2025-04-27 18:51:04 +0100

    Update hw01a.py
    Deleted the code comments which were written, below.

--- frameworks/streamlit/hw/hw01a.py
+++ frameworks/streamlit/hw/hw01a.py
@@ -3,20 +3,3 @@
 # --------------------------------
 import streamlit as st
 st.write("Hello. world!")
-# --------------------------------
-# NOTE: For the above example code to work...
-#       type into the Linux Mint OS Terminal command:
-#       streamlit run hw01.py
-#       ...next, go over to your web browser software,
-#       and, type in the following URL into the web browser address bar:
-#       localhost:5001
-#       ...the web page will load showing:
-#       - a mark down size 1 heading: My first app
-#       ...and, with output text below...
-#       Hello, world! From Streamlit
-#       You can shut the web server down by typing:
-#       [CTRL]+[C]
-#       NOTE: If you 'started' up your browser by calling the Python script;
-#             then, automatically, it will close the web browser down.
-#             If your web browser was already open with tabs before...;
-#             then, everything should still remain open.
