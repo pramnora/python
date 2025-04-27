5968523e399ad87973791820e70037bb1968805c
Paul Ramnora <pramnora@yahoo.com>
2025-04-27 02:00:01 +0100

    Create factorial01a.py
    Initial commit. Created file: [factorial01a.py] Borrowed code...shows how to calculate factorial...uses the number 5.

---
+++ programs/maths/factorial/factorial01a.py
@@ -0,0 +1,18 @@
+# CREATED: Fri 250425 04:32 PM GMT
+# UPDATED: Fri 250425 04:32 PM GMT
+# --------------------------------
+# Code borrowed from...
+# https://www.youtube.com/shorts/lo9ErkgqLMU
+
+nums=[1,2,3,4,5]
+product = 1
+for num in nums:
+    product *= num
+print(product)
+
+# Prod * LoopNum = Prod
+# 1 x 1 = 1
+# 1 x 2 = 2
+# 2 x 3 = 6
+# 6 x 4 = 24
+# 24 x 5 = 120
