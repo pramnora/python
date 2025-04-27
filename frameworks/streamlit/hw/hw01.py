e2243ab591fbc6f1e75130a822871d06532fcc51
Paul Ramnora <pramnora@yahoo.com>
2025-04-27 05:37:57 +0100

    Create hw01.py
    Initial commit. Created file: [hw01.py] My 1st streamlit 'Hello, world' program...

---
+++ frameworks/streamlit/hw/hw01.py
@@ -0,0 +1,8 @@
+# CREATED: Sun 270425 04:36 AM GMT
+# UPDATED: Sun 270425 04:36 AM GMT
+# --------------------------------
+import streamlit as st
+st.write("""
+# My first app
+Hello, world! From Streamlit
+""")
