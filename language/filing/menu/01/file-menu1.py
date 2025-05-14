64271d8854ebcb20660909a1b700cc0007323a6f
Paul Ramnora <pramnora@yahoo.com>
2025-05-14 13:34:10 +0100

    Update file-menu1.py
    Updated bugs found list...with 2 examples.

--- language/filing/menu/01/file-menu1.py
+++ language/filing/menu/01/file-menu1.py
@@ -13,7 +13,13 @@
 
 # ---------------------------------------------------
 # BUGS FOUND LIST...
-# 
+# 1. When the program is first run...; 
+#    one needs to give it a file name to work with;
+#    otherwise, the program bugs out.
+# 2. Also, after giving the program a file name;
+#    one has to write to it...;
+#    otherwise, the program will complain can't find file
+#    to read/append to.    
 # ---------------------------------------------------
 
 # initialise variables...
