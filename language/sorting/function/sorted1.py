6b6a6b3a57b646f71c5f595e375d142900955abd
Paul Ramnora <pramnora@yahoo.com>
2025-05-31 20:43:28 +0100

    Create sorted1.py
    Created file: [sorted1.py]. Demonstrates how to use Python's built-in: sorted() function...; which can be used to work with either numbers/letters/strings.

---
+++ language/sorting/function/sorted1.py
@@ -0,0 +1,40 @@
+# ----------------------------
+# CREATED: 310525 20:26 PM GMT
+# UPDATED: 310525 20:26 PM GMT
+# ----------------------------
+
+# NOTES
+# -----
+
+# The aim of this program is to sort...;
+# by using Python's built-in 'sorted()' function.
+
+# ---------------
+# Main program...
+# ---------------
+
+# declara variables...
+
+numbers_list = [2,3,1,3]
+alphabet_list = ['c','b','a','b']
+
+# print list of numbers...unsorted/then, sorted...
+
+print("Numbers series...")
+
+print("This is the unsorted list: ")
+print(numbers_list)         # unsorted list
+
+print("This  is the sorted list: ")
+print(sorted(numbers_list)) # sorted list
+
+# print(sorted(list2))
+
+print("\nLetters series...") # \n = new line character
+
+print("This is the unsorted list: ")
+print(alphabet_list)         # unsorted list
+
+print("This  is the sorted list: ")
+print(sorted(alphabet_list)) # sorted list
+
