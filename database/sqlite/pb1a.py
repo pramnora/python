48c280f6b1a21253e829ac64cb930133dc7507bc
Paul Ramnora <pramnora@yahoo.com>
2025-06-17 12:52:39 +0100

    Update pb1a.py
    Inserted a number of comments...; to help reading the code become, hopefully, a lot more clear.

--- database/sqlite/pb1a.py
+++ database/sqlite/pb1a.py
@@ -15,13 +15,19 @@
 
 # ----------------------------------------------------------------------
 
+# Create SQLite database...
+
 import sqlite3
-conn = sqlite3.connect('pb1.db') 
+conn = sqlite3.connect('pb1.db') # open database
+
+# Add a number of fields...
 
 conn.execute('''CREATE TABLE PHONEBOOK(
 ID INT PRIMARY KEY NOT NULL,
 NAME CHAR(50),
 NUMBER CHAR(20));''')
+
+# Insert data into fields...
 
 conn.execute("INSERT INTO PHONEBOOK (ID, NAME, NUMBER) \
 VALUES (1,'Jack','0207 111 1111')");  
@@ -29,12 +35,14 @@ conn.execute("INSERT INTO PHONEBOOK (ID, NAME, NUMBER) \
 VALUES (2,'Jill','0208 222 2222')");  
 conn.commit()
 
+# Print database...including, all records/all fields...
+
 cursor=conn.execute("SELECT ID, NAME, NUMBER FROM PHONEBOOK")
 print("ID,NAME,NUMBER")
 for row in cursor:
         print(f"{row[0]},{row[1]},{row[2]}")
 
-conn.close()
+conn.close() # close database
 
 # ----------------------------------------------------------------------
 
