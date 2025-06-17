d0542a569b9b72d1e686bc847f0f6fdc21ac8f91
Paul Ramnora <pramnora@yahoo.com>
2025-06-17 13:34:33 +0100

    Create pb1b.py
    Version 2/Deleted Version 1 code comments

---
+++ database/sqlite/pb1b.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sqlite3                  
+conn = sqlite3.connect('pb1.db') 
+
+conn.execute('''CREATE TABLE PHONEBOOK(
+ID INT PRIMARY KEY NOT NULL,
+NAME CHAR(50),
+NUMBER CHAR(20));''')
+
+conn.execute("INSERT INTO PHONEBOOK (ID, NAME, NUMBER) \
+VALUES (1,'Jack','0207 111 1111')");
+conn.execute("INSERT INTO PHONEBOOK (ID, NAME, NUMBER) \
+VALUES (2,'Jill','0208 222 2222')");  
+conn.commit()
+
+cursor=conn.execute("SELECT ID, NAME, NUMBER FROM PHONEBOOK") 
+print("ID,NAME,NUMBER") 
+for row in cursor:      
+        print(f"{row[0]},{row[1]},{row[2]}")
+conn.close() # close database
