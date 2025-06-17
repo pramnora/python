598009873f72d3c40047eeeb1f11b4cd68b3e784
Paul Ramnora <pramnora@yahoo.com>
2025-06-17 12:10:47 +0100

    Update pb1a.py
    Re-wrote the file in parts...to read more clear...; also, included an 'output' section.

--- database/sqlite/pb1a.py
+++ database/sqlite/pb1a.py
@@ -1,9 +1,10 @@
 !/usr/bin/python3
 
-# Program creates a very simple phonebook database...;
-# adds 3 database fields: id,name,number
-# next adds 2 separate records...each record containing the 3 fields
-# finally prints out all records/together with all fields
+# Program creates a very simple phonebook database...;  
+# the database file is called: [pb1.db];  
+# adds 3 database fields: id,name,number  
+# next, adds 2 separate records: Jack/Jill...with each record containing each of the 3 above fields;  
+# finally prints out all records/together with all fields  
 
 import sqlite3
 conn = sqlite3.connect('pb1.db') 
@@ -14,9 +15,9 @@ NAME CHAR(50),
 NUMBER CHAR(20));''')
 
 conn.execute("INSERT INTO PHONEBOOK (ID, NAME, NUMBER) \
-VALUES (1,'Jack','0208 111 1111')");  
+VALUES (1,'Jack','0207 111 1111')");  
 conn.execute("INSERT INTO PHONEBOOK (ID, NAME, NUMBER) \
-VALUES (2,'Jill','0207 222 2222')");  
+VALUES (2,'Jill','0208 222 2222')");  
 conn.commit()
 
 cursor=conn.execute("SELECT ID, NAME, NUMBER FROM PHONEBOOK")
@@ -25,5 +26,10 @@ for row in cursor:
         print(f"{row[0]},{row[1]},{row[2]}")
 
 conn.close()
+
+# output...
+# ID,NAME,NUMBER
+# 1,Jack,0207 111 1111
+# 2,Jill,0208 222 2222
 
 
