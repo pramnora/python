f3022ab633ca3419e8ff7d24718c3e266e03e3c5
Paul Ramnora <pramnora@yahoo.com>
2025-07-09 07:40:49 +0100

    Create hw01.py
    My first TCL Hello, world! program...

---
+++ other/language/tcl/hw/hw01.py
@@ -0,0 +1,5 @@
+# CREATED: 080725 21:00 PM GMT
+# UPDATED: 080725 21:00 PM GMT
+# ----------------------------
+import tkinter as tk
+tk.Tcl().eval('puts {Hello, world! From Tcl(pronounced tickle)}')
