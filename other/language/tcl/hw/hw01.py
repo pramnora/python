4405ee0afa22d3d0b3170e100325c5394255b418
Paul Ramnora <pramnora@yahoo.com>
2025-07-09 07:45:46 +0100

    Update hw01.py
    Added comment line: Tcl - Tool Command Language

--- other/language/tcl/hw/hw01.py
+++ other/language/tcl/hw/hw01.py
@@ -2,4 +2,5 @@
 # UPDATED: 080725 21:00 PM GMT
 # ----------------------------
 import tkinter as tk
+# Tcl - Tool Command Language 
 tk.Tcl().eval('puts {Hello, world! From Tcl\(pronounced tickle)}')
