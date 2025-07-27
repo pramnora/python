c451cdd8c974eb85551b717090619d37703ea592
Paul Ramnora <pramnora@yahoo.com>
2025-07-27 05:42:25 +0100

    Update tk-msgbox-warn.py
    re-wrote message box title/text

--- library/tk/language/messagebox/tk-msgbox-warn.py
+++ library/tk/language/messagebox/tk-msgbox-warn.py
@@ -4,4 +4,4 @@ from tkinter import messagebox
 root = Tk()
 root.withdraw()
 
-messagebox.showwarning("Tkinter Window","This is a warning message box!")
+messagebox.showwarning("Tkinter: Warning message box","This is a warning...!")
