c2045de0aa2dab12382e537db789af21c07dcb8a
Paul Ramnora <pramnora@yahoo.com>
2025-07-29 14:07:13 +0100

    Create lambda-05.py
    Created file: [lambda-05.py] How to use anonymous lambda function...

---
+++ language/functions/lambda/lambda-05.py
@@ -0,0 +1,13 @@
+# Demo: How to use anonymous lambda...
+
+# create anonymous function
+def myfunc(n):
+    return lambda a:a*n
+
+# initialise a
+mydoubler=myfunc(2)
+mytripler=myfunc(3)
+
+# initialise n
+print(mydoubler(2))
+print(mytripler(2))
