252a134e05abf720e2f623726d3f7636a2168d45
Paul Ramnora <pramnora@yahoo.com>
2025-07-29 14:14:43 +0100

    Update lambda-04.py
    Minor edit: Added vertical spacing

--- language/functions/lambda/lambda-04.py
+++ language/functions/lambda/lambda-04.py
@@ -1,8 +1,10 @@
 # Demo: Lambda, how to use 'default' arguments...
 
 # create function
+
 double=lambda y=3:y*2 # setting a 'default' argument
 
 # make function calls
+
 print(double())  # outputs: 6 (2*3)/using function 'default' value
 print(double(4)) # outputs: 8 (2*4)/using function call value 
