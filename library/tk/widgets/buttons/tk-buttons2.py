93e7fd76664fcc5eeca0cfaab98073804de57839
Paul Ramnora <pramnora@yahoo.com>
2025-07-29 16:52:50 +0100

    Update tk-buttons2.py
    Updated file: added root.geometry("300x300") to set the windows size: width x height.

--- library/tk/widgets/buttons/tk-buttons2.py
+++ library/tk/widgets/buttons/tk-buttons2.py
@@ -4,10 +4,14 @@ root=tk.Tk()
 
 root.title("Buttons...")
 
+root.geometry("300x300") # set window width x height
+
 def myButton1():
     print("Button1 clicked!")
+    
 def myButton2():
     print("Button2 clicked!")
+    
 def myButton3():
     print("Button3 clicked!")
 
@@ -20,11 +24,13 @@ button1.pack(padx=10,pady=10)
 button2=tk.Button(root,
                   text="Button 2",
                   command=myButton2)
+
 button2.pack(padx=10,pady=10)
 
 button3=tk.Button(root,
                   text="Button 3",
                   command=myButton3)
+
 button3.pack(padx=10,pady=10)
 
 root.mainloop()
