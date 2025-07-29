66f509f6f4bfaf26a46f812afb6bc5f1e86093ec
Paul Ramnora <pramnora@yahoo.com>
2025-07-29 16:58:03 +0100

    Update tk-buttons2.py
    Re-arranged code...

--- library/tk/widgets/buttons/tk-buttons2.py
+++ library/tk/widgets/buttons/tk-buttons2.py
@@ -1,10 +1,18 @@
+# import tkinter library
 import tkinter as tk
 
-root=tk.Tk()
+# ---------------------------------------------------
 
-root.title("Buttons...")
+# set window properties
 
+root=tk.Tk()             # create window
+root.title("Buttons...") # set window title text
 root.geometry("300x300") # set window width x height
+
+# ---------------------------------------------------
+
+# Create functions for 3 separate buttons...;
+# prints out a message when each button is clicked.
 
 def myButton1():
     print("Button1 clicked!")
@@ -15,22 +23,19 @@ def myButton2():
 def myButton3():
     print("Button3 clicked!")
 
-button1=tk.Button(root,
-                  text="Button 1",
-                  command=myButton1,)
+# ---------------------------------------------------
 
+# Create 3 buttons... 
+
+button1=tk.Button(root, text="Button 1",command=myButton1)
 button1.pack(padx=10,pady=10)
 
-button2=tk.Button(root,
-                  text="Button 2",
-                  command=myButton2)
-
+button2=tk.Button(root, text="Button 2",command=myButton3)
 button2.pack(padx=10,pady=10)
 
-button3=tk.Button(root,
-                  text="Button 3",
-                  command=myButton3)
-
+button3=tk.Button(root, text="Button 2",command=myButton3)
 button3.pack(padx=10,pady=10)
 
-root.mainloop()
+# ---------------------------------------------------
+
+root.mainloop() # create a window running loop
