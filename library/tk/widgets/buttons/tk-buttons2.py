e38ea7985cd0cf6bbdd50c579eac7cdf874afb2d
Paul Ramnora <pramnora@yahoo.com>
2025-07-29 16:46:42 +0100

    Create tk-buttons2.py
    Created file: [tk-buttons2.py] Creates 3 buttons: Button1, Button2, Button3...which when clicked on prints out the text: Button 1 clicked/-etc.

---
+++ library/tk/widgets/buttons/tk-buttons2.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+root=tk.Tk()
+
+root.title("Buttons...")
+
+def myButton1():
+    print("Button1 clicked!")
+def myButton2():
+    print("Button2 clicked!")
+def myButton3():
+    print("Button3 clicked!")
+
+button1=tk.Button(root,
+                  text="Button 1",
+                  command=myButton1,)
+
+button1.pack(padx=10,pady=10)
+
+button2=tk.Button(root,
+                  text="Button 2",
+                  command=myButton2)
+button2.pack(padx=10,pady=10)
+
+button3=tk.Button(root,
+                  text="Button 3",
+                  command=myButton3)
+button3.pack(padx=10,pady=10)
+
+root.mainloop()
