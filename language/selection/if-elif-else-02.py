cb96e7ccb517b0b382e338e645ed3540bf154620
Paul Ramnora <pramnora@yahoo.com>
2025-08-19 16:35:41 +0100

    Create if-elif-else-02.py
    Created file: [if-elif-else.py] Python selection using: if

---
+++ language/selection/if-elif-else-02.py
@@ -0,0 +1,16 @@
+nums=[0,1,2,3]
+
+# print headers...
+
+print("X - Result")
+print("-   ------")
+
+# use for loop to check values/results...
+
+for x in nums:
+    if x==1:
+       print(x,"- x=1")
+    elif x==2:
+       print(x,"- x=2")
+    else:
+       print(x,"- x!=1 & x!=2")
