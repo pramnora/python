165839b3e8287153c7cba77b79f2b533b759704d
Paul Ramnora <pramnora@yahoo.com>
2026-06-19 14:58:38 +0100

    Implement dice roller using random number generation
    Added a simple dice roller that generates a random number between 1 and 6.

---
+++ projects/math/random/dice/01/dice.py
@@ -0,0 +1,8 @@
+import random               # import random
+                            # which is a 'built-in' Python library
+                            # that can be used to produce random numbers
+
+print(random.randint(1,6))  # print a single dice, 
+                            # using method: 'random.randint(minNo,maxNo)'
+                            # to create a dice displaying a random number: 1-6
+
