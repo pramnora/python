66905c65ba8aa636748eef425fd754fdb26f6f21
Paul Ramnora <pramnora@yahoo.com>
2026-06-19 18:31:24 +0100

    Add word-dice.py to simulate dice rolls with words

---
+++ projects/math/random/dice/02/word-dice.py
@@ -0,0 +1,12 @@
+import random
+
+diceNo=random.randint(1,6)
+
+nums=["one","two","three","four","five","six"]
+
+if diceNo == 1: print(nums[diceNo-1])
+if diceNo == 2: print(nums[diceNo-1])
+if diceNo == 3: print(nums[diceNo-1])
+if diceNo == 4: print(nums[diceNo-1])
+if diceNo == 5: print(nums[diceNo-1])
+if diceNo == 6: print(nums[diceNo-1])
