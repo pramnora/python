c83cd7530bca67c411d80ec2b61ed20f0ae63835
Paul Ramnora <pramnora@yahoo.com>
2026-06-19 20:33:35 +0100

    Correct loop range and print corresponding dice word
    Fix the loop range and improve readability.

--- projects/math/random/dice/02/word-diceB.py
+++ projects/math/random/dice/02/word-diceB.py
@@ -1,5 +1,9 @@
 import random
+
 diceNo=random.randint(1,6)
+
 nums=["one","two","three","four","five","six"]
+
 for eachLoopNo in range(1,6+1):
+    
     if eachLoopNo == diceNo: print(nums[diceNo-1])
