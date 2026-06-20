488c8279e2dd96925496de2f899282de917c0c8a
Paul Ramnora <pramnora@yahoo.com>
2026-06-20 10:20:38 +0100

    Add bar chart visualization in bar1.py

---
+++ library/external/matplotlib/graph/bar-chart/bar1.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt         # import pyplot from matplotlib as alias, plt
+
+# bar chart
+
+x=["Eggs","Tomatoes","Bread","Milk"]    # x: horizontal
+y=[30,8,4,2]                            # y: vertical
+
+plt.title("Food brought this month...") # title text
+plt.xlabel("Food")                      # x label
+plt.ylabel("Amount")                    # y label
+
+plt.bar(x,y)                            # draw bar chart
+plt.show()                              # show bar chart
