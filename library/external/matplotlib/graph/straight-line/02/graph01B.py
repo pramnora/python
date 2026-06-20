4ebd99a5dc468c44a4f25eac24df7e8df1547741
Paul Ramnora <pramnora@yahoo.com>
2026-06-20 03:00:23 +0100

    Add graph01B.py to plot straight line graph
    This script generates a straight line graph using matplotlib, plotting months on the x-axis and corresponding values on the y-axis.

---
+++ library/external/matplotlib/graph/straight-line/02/graph01B.py
@@ -0,0 +1,40 @@
+# -------------------------------------------------  
+# This code borrowed from  
+# YouTube Channel: Bro Code  
+# https://www.youtube.com/watch?v=c9vhHUGdav0  
+# -------------------------------------------------  
+# NOTE: His own code was simpler.../  
+#       and, therefore, was uniquely adapted by me.  
+# -------------------------------------------------  
+# CREATED: 200626 02:18 AM GMT  
+# UPDATED: 200626 03:00 AM GMT  
+# -------------------------------------------------  
+
+# Explanation:  
+
+# This code outputs a straight line graph...;  
+# which shows up in the form of numerous V shapes;  
+# that extend going across the page, horizontally.  
+
+# This is because the data both grows and shrinks in size,  
+# ranging from the lowest point: 2;  
+# and, going up to the highest point: 25.  
+
+# -(In my next version B...;  
+# I have deliberately switched the axes around:    
+# y to become x/x to become y;     
+# so, in that particular case,   
+# the data is being plotted going vertically upwards, instead.)-    
+
+# ==================================================  
+
+import matplotlib.pyplot as plt  
+
+y=[5,10,15,20,11,12,14,6,18,25,2,8]  
+x=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]  
+
+# NOTE: All I did to the above code was simply change: x to say y/y to say x;
+#       and, this change will output instead of horizontal lines/vertical lines.
+
+plt.plot(x,y)  
+plt.show()  
