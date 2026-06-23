2e1bea14a72f8c81b9769d43cb0c9a21cb5f6a53
Paul Ramnora <pramnora@yahoo.com>
2026-06-23 17:18:42 +0100

    Enhance vowel finder to count uppercase letters
    Updated vowel counting logic to include uppercase letters and added counts for total letters, consonants, and other characters.

--- programs/text/vowel-finder-01.py
+++ programs/text/vowel-finder-01.py
@@ -7,25 +7,47 @@
 #       EMAIL: pramnora@yahoo.com
 #   COPYRIGHT: (c)2018-, Mr. Paul Ramnora./All rights reserved.
 #--------------------------------------------------------------
-#     CREATED: TUE 4 DEC 2018 - 20:56 PM GMT
-#     UPDATED: TUE 4 DEC 2018 - 20:56 PM GMT
+#     CREATED: TUE 04 DEC 2018 - 20:56 PM GMT
+#     UPDATED: TUE 22 JUN 2026 - 17:18 PM GMT
 #--------------------------------------------------------------
 
 #*** BUG LIST FOUND...
 #    1. The program only checks to find 'lower case' letters; but, does NOT check to find 'upper case' letters?
 
-vowels="aeiou"
-aCount=eCount=iCount=oCount=uCount=0
+aCount=eCount=iCount=oCount=uCount=vowelsCount=consonantsCount=lettersCount=otherCount=0
+alphabetLetters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 print("PROGRAM: Vowel count\n")
-aString=input("Please, enter a text string: ")
+print("Please enter a text string:")
+aString=input()
 for eachChar in aString:
     if eachChar in vowels:
-        if eachChar == "a": aCount+=1
-        if eachChar == "e": eCount+=1
-        if eachChar == "i": iCount+=1
-        if eachChar == "o": oCount+=1
-        if eachChar == "u": uCount+=1
-
-print(len(aString),"characters")
+        if eachChar == "a": 
+           aCount+=1
+           vowelsCount+=1
+        if eachChar == "e": 
+           eCount+=1 
+           vowelsCount+=1
+        if eachChar == "i": 
+           iCount+=1
+           vowelsCount+=1
+        if eachChar == "o": 
+           oCount+=1
+           vowelsCount+=1
+        if eachChar == "u": 
+           uCount+=1
+           vowelsCount+=1
+    else:
+        consonantsCount+=1
+    if eachChar.upper() in alphabetLetters: 
+        lettersCount+=1
+    else:
+      otherCount+=1
 print("a","e","i","o","u")
+
 print(aCount,eCount,iCount,oCount,uCount)
+print(len(aString),"characters. (Including spaces/punctuation symbols/-etc.)")
+print("   Letters: ",lettersCount)
+print("Consonants: ",consonantsCount)
+print("    Vowels: ",vowelsCount)
+print("     Other: ",otherCount)
+
