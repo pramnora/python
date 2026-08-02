912d829014a1d0ab904c1255625252a194cf04b9
Paul Ramnora <pramnora@yahoo.com>
2026-08-02 13:43:20 +0100

    Add basic Gemini v1 content generation script

---
+++ others-code/chatbot/google/genai/basic-gemini-v1.py
@@ -0,0 +1,12 @@
+
+
+    from google import genai
+
+    client=genai.Client(api_key="MY_SECRET_API-KEY")
+
+    response=client.models.generate_content(
+        model="gemini-3.5-flash",
+        contents="Explain Python to me!"
+    )
+
+    print(response.text)
