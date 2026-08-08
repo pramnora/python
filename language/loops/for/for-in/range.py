b72c7a211824d6f18c7d0c51153b1abbe41226d0
Paul Ramnora <pramnora@yahoo.com>
2026-08-08 06:43:35 +0100

    Refactor print statements for horizontal output
    Updated print statements to use 'end' parameter for horizontal output.

--- language/loops/for/for-in/range.py
+++ language/loops/for/for-in/range.py
@@ -1,20 +1,22 @@
-for x in range(10): #the count starts at 0...and, finishes -1 from the loop specified count...eg. 0-9 
-    print (x), #the comma at the end prints horizontally across...with a space showing in between each number 
-              
+for x in range(10):   # the count starts at 0...and, finishes -1 from the loop specified count...eg. 0-9 
+    print (x,end=" ") # the end=" " prints horizontally across/with a space showing in between each number 
+print()               # reset cursor              
 # 0 1 2 3 4 5  6 7 8 9
 
-for x in range(1,10): #this sets the count to start from 1...and, end at 9...= 10 (-1)
-    print (x),
+for x in range(1,10): # this sets the count to start from 1...and, end at 9...= 10 (-1)
+    print (x,end=" ") # the end=" " prints horizontally across/with a space showing in between each number 
+print()               # reset cursor              
     
 #1 2 3 4 5 6 7 8 9
 
-for x in range(1,10,2): #this sets the step of the count...count up in steps of 2 at a time
-    print (x),
+for x in range(1,10,2): # this sets the step of the count...count up in steps of 2 at a time
+    print (x,end=" ")   # the end=" " prints horizontally across/with a space showing in between each number 
+print()                 # reset cursor              
     
 #1 3 5 7 9
 
 for x in range(0,10,2):
-	print (x) #no comma at the end prints out vertically
+	print (x)        # normal print/prints with new line at the end
     
 #0
 #2
@@ -27,5 +29,6 @@ for x in range(0,10,2):
 # Update: 080826 05:06 AM GMT 
 
 for x in range(10,0,-1):
-	print (x,end=" ") # prints numbers counting backwards according to the step count number: -1
+    print (x,end=" ") # the end=" " prints horizontally across/with a space showing in between each number 
+print()               # reset cursor              
 # 10 9 8 7 6 5 4 3 2 1
